3314891cc71d7456adfe88dc84ca7dd0a0f5a931
mengrui song <summerrock1017@gmail.com>
2022-11-21 00:20:31 +0900

    normalize email address

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,4 +128,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'core.User' # setting Auth_User_Model configuration
+AUTH_USER_MODEL = 'core.User'
+# setting Auth_User_Model configuration
